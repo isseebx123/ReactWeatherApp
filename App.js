9f3e1b8a93f44ccf1ca4a707a1a359bbbcb20871
SangHyun Lee <lsseebx38@gmail.com>
2018-01-13 16:03:54 +0900

    fin Handling Eror on Geolocation

--- App.js
+++ App.js
@@ -4,16 +4,25 @@ import Weather from "./Weather"
 
 export default class App extends React.Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null
   };
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(position);
-      });
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        this.setState({
+          error: error
+        })
+      }
+    );
   }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
@@ -22,6 +31,7 @@ export default class App extends React.Component {
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -33,6 +43,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  errorText:{
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
     flex: 1,

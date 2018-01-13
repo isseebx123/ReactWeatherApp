559b7b71bf5f6eb4e72d218a7785fa885f3a63a7
SangHyun Lee <lsseebx38@gmail.com>
2018-01-13 14:38:21 +0900

    fin Building the Loading View

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false
+  };
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the weather</Text>
+          </View>)}
       </View>
     );
   }
@@ -16,17 +22,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent:'center',
-    alignItems:'center'
   },
-  redView:{
-    height:50,
-    width:50,
-    backgroundColor:'red'
+  loading: {
+    flex: 1,
+    backgroundColor:'#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
   },
-  yellowView:{
-    height:50,
-    width:50,
-    backgroundColor:'yellow'
+  loadingText:{
+    fontSize:38,
+    marginBottom: 100
   }
 });
